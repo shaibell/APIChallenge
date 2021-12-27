5280cf31aa0d4eb85485455916bff77d4cea99bb
Shai Bell <shairbell@gmail.com>
2021-12-27 11:46:54 -0500

    finished api

--- script.js
+++ script.js
@@ -6,21 +6,75 @@ take that name, and fetch the pokemon data with it
 display the pokemon data in the document
 */
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
-   
-.then(response => {
-        console.log(response);
-        return response.json()
-    })
-        .then(jsonData => {
-            console.log(jsonData);
-        });
+const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+// let pokemonName = 'butterfree'
 
-        let buttonElement = document.getElementById(lookUp)
+
+function fetchPokemon() {
+    let pokemon = document.getElementById("pokemonName").value;
+
+    let url = `${ baseURL }${ pokemon }`;
+
+   
+
+    
+fetch(url)  
+
+.then(response => {
+ //       console.log(response);
+      return  response.json()
+    })
+
+
+       .then(jsonData => {
+         console.log(jsonData.abilities[0].ability.name)
+
+
+        let PokemonInfo = document.getElementById('results-returned');
+
+        let moves = document.createElement("p")
+        PokemonInfo.appendChild(moves)
+        let DisplayData2 = jsonData.moves[0].move.name
+        moves.innerText = DisplayData2;
+        //PokemonInfo.innerText = 'coffee'
         
+        let ability = document.createElement("p")
+        PokemonInfo.appendChild(ability)
+        let DisplayData = jsonData.abilities[0].ability.name
+        ability.innerText = DisplayData
+        });
+    }
+
+
 
         let myButton = document.getElementById('lookUp');
 
-myInputBox.addEventListener('onChange')
-myButton.addEventListener('click', (event) => {
-    console.log(event.target.value)})
+      
+
+//myInputBox.addEventListener('onChange')
+//myButton.addEventListener('click', (event) => {
+  //  console.log(event.target.value)})
+  const myInput = document.getElementById('pokemonName');
+ 
+
+  myInput.addEventListener('onChange', (event) => {
+      userInput = event.target.value;
+      console.log(event)
+  });
+
+
+  myButton.addEventListener('click', fetchPokemon  //run pokemon, fetch insteead
+  )
+  //let clearbutton = document.getElementById("results-returned") --how to clear results?
+
+clearbutton.addEventListener('click', console.clear());
+ // document.getElementById("results-returned").innerHTML = height;
+ 
+
+
+
+ let PokemonInfo = document.getElementById('results-returned');
+
+//let DisplayData =
+//PokemonInfo.innerText = DisplayData  // there is an issue at this line, causing line 34 to not display on webpage
+
